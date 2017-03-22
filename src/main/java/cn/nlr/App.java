e1d7d073b32d4ff735354e97feae8a71e67b7373
dlwangwj <dlwangwj@cn.ibm.com>
2017-03-22 11:15:43 +0800

    添加异常Whitelabel Error Page处理方式

--- src/main/java/cn/nlr/App.java
+++ src/main/java/cn/nlr/App.java
@@ -13,6 +13,17 @@ public class App extends SpringBootServletInitializer {
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(App.class);
 	}
+	//@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer() {
+//
+//	   return (container -> {
+//	        ErrorPage error401Page = new ErrorPage(HttpStatus.UNAUTHORIZED, "/401.html");
+//	        ErrorPage error404Page = new ErrorPage(HttpStatus.NOT_FOUND, "/404.html");
+//	        ErrorPage error500Page = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/500.html");
+//
+//	        container.addErrorPages(error401Page, error404Page, error500Page);
+//	   });
+//	}
     public static void main( String[] args )
     {
     	 SpringApplication application = new SpringApplication(App.class);
